58ec2b64e3e473b535418983c5809934b941e6dc
LAPTOP-KQ06NIAA\liu <eb.liujuntao@qq.com>
2025-03-15 22:06:58 +0800

    setup:

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_requirements(source):
     """
 
     install_reqs = parse_requirements(filename=source, session=PipSession())
-
+    print ("{ir}")
     return [str(ir.req) for ir in install_reqs]
 
 
